f63ac4d703d4f72e99bddb1f342756c44d574176
Nyirabazungu <vnyanki6@gmail.com>
2019-03-29 08:30:08 +0200

    add test

--- images/tests.py
+++ images/tests.py
@@ -1,55 +1,50 @@
-# from django.test import TestCase
-# from .models import Images,Profile
+from django.test import TestCase
+from .models import Images,Profile
 
-# # Create your tests here.
-# class CategoryTestClass(TestCase):
+# Create your tests here.
+class ImageTestClass(TestCase):
 
-#     # Set up method
-#     def setUp(self):
-#         self.love= Images(title = 'Love', description ='Muriuki',category='',location='')
+    # Set up method
+    def setUp(self):
+        self.love= Images(title = 'Love',  caption ='If you love me, i love you too')
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.love,Images))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.love,Images))
 
-#     def test_save_method(self):
-#         self.love.save_images()
-#         images = Images.objects.all()
-#         self.assertTrue(len(images) > 0)   
+    def test_save_method(self):
+        self.love.save_images()
+        images = Images.objects.all()
+        self.assertTrue(len(images) > 0)   
 
 
-#     def test_delete_method(self):
-#         self.love.save_images()
-#         images = Image.objects.all()
-#         self.assertTrue(len(images) > 0)   
+    def test_delete_method(self):
+        self.love.save_images()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)   
 
-#     def test_display_method(self):
-#         self.love.save_images()
-#         images = Images.objects.all()
-#         self.assertTrue(len(images) > 0)
+    def test_update_method(self):
+        self.love.save_images()
+        images = Images.objects.all()
+        self.assertTrue(len(images) > 0)                
 
-#     def test_update_method(self):
-#         self.love.save_images()
-#         images = Images.objects.all()
-#         self.assertTrue(len(images) > 0)                
+class ProfileTestClass(TestCase):
 
-# class ArticleTestClass(TestCase):
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.james= user(user_name= 'vava',first_name = 'James', last_name ='Muriuki', bio='I have bechor in computer science')
+        self.james.save_user()
 
-#     def setUp(self):
-#         # Creating a new editor and saving it
-#         self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
-#         self.james.save_editor()
+        # Creating a new profileand saving it
+        self.new_profile= profiles(name = 'testing')
+        self.new_profile.save()
 
-#         # Creating a new tag and saving it
-#         self.new_tag = tags(name = 'testing')
-#         self.new_tag.save()
+        self.profile= Profile(title = 'Test Profile',post = 'This is a pictures',user = self.james)
+        self.profile.save()
 
-#         self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
-#         self.new_article.save()
+        self.profile.profiles.add(self.new_profile)
 
-#         self.new_article.tags.add(self.new_tag)
-
-#     def tearDown(self):
-#         Editor.objects.all().delete()
-#         tags.objects.all().delete()
-#         Article.objects.all().delete()    
+    def tearDown(self):
+        User.objects.all().delete()
+        images.objects.all().delete()
+        Profile.objects.all().delete()    
 
